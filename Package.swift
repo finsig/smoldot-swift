79d82a30d25ce7d38665a2879440823a6bed1da1
finsig <steve@finsig.io>
2024-07-25 22:28:49 -0400

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["SmoldotSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/finsig/json-rpc2", from: "0.1.1")
+        .package(url: "https://github.com/finsig/json-rpc2", from: "0.1.1"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
     ],
     targets: [
